ba0618ca8431f822682494b3b647310b3af598b1
Chad Fairlie <104318840+AlphaSweater@users.noreply.github.com>
2023-06-08 17:48:09 +0200

    .cat

--- src/main/java/com/alphasweater/MainClass.java
+++ src/main/java/com/alphasweater/MainClass.java
@@ -7,12 +7,6 @@ package com.alphasweater;
 import com.alphasweater.MyGUI.MyLoginWorkerClass;
 
 public class MainClass {
-
-    /**
-     * The main method that starts the application by creating the login page.
-     *
-     * @param args The command-line arguments.
-     */
     public static void main(String[] args) {
         MyLoginWorkerClass.createLoginPage();
     }
