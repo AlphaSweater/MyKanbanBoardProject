09eea59e4d3898fad31c258aea21a630168622df
Chad Fairlie <Chad.fairlie2004@gmail.com>
2023-07-04 17:42:46 +0200

    Added methods for working with tasks

--- src/main/java/com/alphasweater/MyTasks/MyTaskListController.java
+++ src/main/java/com/alphasweater/MyTasks/MyTaskListController.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 *  Student Number: ST10269509
 ---------------------------------------------------------------------------------------------------------------------*/
 public class MyTaskListController {
-
+    
     //----------------------------------------------------------------------------------------------------------------//
     private int numOfTasks;
     public int getNumOfTasks() {
@@ -22,6 +22,8 @@ public class MyTaskListController {
         return listOfTasks;
     }
     //----------------------------------------------------------------------------------------------------------------//
+//    public final ArrayList<MyTasksClass> listOfCurrentWorkingTasks = new ArrayList<MyTasksClass>();
+    //----------------------------------------------------------------------------------------------------------------//
     public int returnTotalHours(ArrayList<MyTasksClass> listOfTasks) {
         int totalHours = 0;
         for (MyTasksClass task : listOfTasks) {
@@ -29,6 +31,50 @@ public class MyTaskListController {
         }
         return totalHours;
     }
+    //----------------------------------------------------------------------------------------------------------------//
+    public ArrayList<MyTasksClass> findAllDoneTasks (ArrayList<MyTasksClass> listOfTasks){
+        ArrayList<MyTasksClass> listOfTaskStatusDone = new ArrayList<MyTasksClass>();
+        for (MyTasksClass task : listOfTasks) {
+            if (task.getTaskStatus().equals("Done")) {
+                listOfTaskStatusDone.add(task);
+            }
+        }
+        return listOfTaskStatusDone;
+    }
+    //----------------------------------------------------------------------------------------------------------------//
+    public MyTasksClass findLongestTask (ArrayList<MyTasksClass> listOfTasks){
+        int longest = listOfTasks.get(0).getTaskDuration();
+        MyTasksClass longestTask = listOfTasks.get(0);
+
+        for (MyTasksClass task : listOfTasks) {
+            if (task.getTaskDuration() > longest){
+                longest = task.getTaskDuration();
+                longestTask = task;
+            }
+        }
+        return longestTask;
+    }
+    //----------------------------------------------------------------------------------------------------------------//
+    public MyTasksClass SearchForTask (ArrayList<MyTasksClass> listOfTasks, String taskName){
+        for (MyTasksClass task : listOfTasks) {
+            if (task.getTaskName().equals(taskName)){
+                return task;
+            }
+        }
+        return null;
+    }
+    //----------------------------------------------------------------------------------------------------------------//
+    public ArrayList<MyTasksClass> FindAllDevsTasks (ArrayList<MyTasksClass> listOfTasks, String DevName){
+        ArrayList<MyTasksClass> listOfDevsTasks = new ArrayList<MyTasksClass>();
+        for (MyTasksClass task : listOfTasks) {
+            if (task.getTaskDevInfo().equals(DevName)){
+                listOfDevsTasks.add(task);
+            }
+        }
+        return listOfDevsTasks;
+    }
+    //----------------------------------------------------------------------------------------------------------------//
+    public void
 }
 //--------------------------------------------------------------------------------------------------------------------//
 //--------------------------------------------------------EOF---------------------------------------------------------//
