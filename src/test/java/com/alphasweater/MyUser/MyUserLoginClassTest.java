d884165670261f18fb219720dc41019ce4fc0048
Chad Fairlie <chad.fairlie2004@gmail.com>
2023-07-07 16:28:02 +0200

    Split tests into multiple methods.

--- src/test/java/com/alphasweater/MyUser/MyUserLoginClassTest.java
+++ src/test/java/com/alphasweater/MyUser/MyUserLoginClassTest.java
@@ -1,10 +1,13 @@
 package com.alphasweater.MyUser;
-/* Author: Chad Fairlie
-*  Pseudonym: AlphaSweater
-*  Student Number: ST10269509
----------------------------------------------------------------------------------------------------------------------*/
+
+/*
+ * Author: Chad Fairlie
+ * Pseudonym: AlphaSweater
+ * Student Number: ST10269509
+ */
 
 import com.alphasweater.MyGUI.MyLoginWorkerClass;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,38 +16,49 @@ import static org.junit.Assert.*;
  * Unit tests for the Login class.
  */
 public class MyUserLoginClassTest {
-    //----------------------------------------------------------------------------------------------------------------//
+    private MyUserLoginClass testLoginUserWorker;
+    private MyLoginWorkerClass testLoginWorker;
+    private String[][] userDatabase;
 
-    /**
-     * Tests the logInUser method.
-     */
-    @Test
-    public void testLogInUser() {
-        MyUserLoginClass testLoginUserWorker = new MyUserLoginClass();
-        MyLoginWorkerClass testLoginWorker = new MyLoginWorkerClass();
+    @Before
+    public void setUp() {
+        // Create the necessary objects and set up the user database before each test
+        testLoginUserWorker = new MyUserLoginClass();
+        testLoginWorker = new MyLoginWorkerClass();
         testLoginUserWorker.setLoginWorker(testLoginWorker);
+
         // Set up a sample user database
-        String[][] userDatabase = {
+        userDatabase = new String[][]{
                 {"username1", "password1", "John", "Doe"},
                 {"username2", "password2", "Jane", "Smith"},
                 {"username3", "password3", "Alice", "Johnson"}
         };
+    }
 
-        // Test for correct username and password
-        boolean result1 = testLoginUserWorker.logInUser("username2", "password2", userDatabase);
-        assertTrue("Login should succeed for a valid username and password.", result1);
+    @Test
+    public void testLogInUser_validUsernameAndPassword_ReturnsTrue() {
+        boolean result = testLoginUserWorker.logInUser("username2", "password2", userDatabase);
+
+        assertTrue("Login should succeed for a valid username and password.", result);
         assertEquals("Welcome Jane Smith, it is great to see you.", testLoginUserWorker.getStatus());
+    }
 
-        // Test for incorrect username
-        boolean result2 = testLoginUserWorker.logInUser("username4", "password1", userDatabase);
-        assertFalse("Login should fail for an incorrect username.", result2);
+    @Test
+    public void testLogInUser_incorrectUsername_ReturnsFalse() {
+        boolean result = testLoginUserWorker.logInUser("username4", "password1", userDatabase);
+
+        assertFalse("Login should fail for an incorrect username.", result);
         assertEquals("Username or password incorrect, please try again or sign up if you do not have an account already.", testLoginUserWorker.getStatus());
+    }
 
-        // Test for incorrect password
-        boolean result3 = testLoginUserWorker.logInUser("username3", "password4", userDatabase);
-        assertFalse("Login should fail for an incorrect password.", result3);
+    @Test
+    public void testLogInUser_incorrectPassword_ReturnsFalse() {
+        boolean result = testLoginUserWorker.logInUser("username3", "password4", userDatabase);
+
+        assertFalse("Login should fail for an incorrect password.", result);
         assertEquals("Username or password incorrect, please try again or sign up if you do not have an account already.", testLoginUserWorker.getStatus());
     }
 }
+
 //--------------------------------------------------------------------------------------------------------------------//
 //--------------------------------------------------------EOF---------------------------------------------------------//
--- src/test/java/com/alphasweater/MyUser/MyUserRegisterClassTest.java
+++ src/test/java/com/alphasweater/MyUser/MyUserRegisterClassTest.java
@@ -1,9 +1,13 @@
 package com.alphasweater.MyUser;
-/* Author: Chad Fairlie
-*  Pseudonym: AlphaSweater
-*  Student Number: ST10269509
----------------------------------------------------------------------------------------------------------------------*/
 
+/*
+ * Author: Chad Fairlie
+ * Pseudonym: AlphaSweater
+ * Student Number: ST10269509
+ */
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,40 +16,58 @@ import static org.junit.Assert.*;
  * Unit tests for the SignUp class.
  */
 public class MyUserRegisterClassTest {
-    //----------------------------------------------------------------------------------------------------------------//
-    /**
-     * Tests the registerUser method.
-     */
-    @Test
-    public void testRegisterUser() {
-        MyUserRegisterClass testRegistrationWorker = new MyUserRegisterClass();
-        // Set up a sample user database
-        String[][] userDatabase = {
+    private MyUserRegisterClass testRegistrationWorker;
+    private String[][] userDatabase;
+
+    @Before
+    public void setUp() {
+        // Create the necessary objects and set up the user database before each test
+        testRegistrationWorker = new MyUserRegisterClass();
+        userDatabase = new String[][]{
                 {"us_r1", "P@ssword1", "John", "Doe"},
                 {"us_r2", "P@ssword2", "Jane", "Smith"},
                 {"us_r3", "P@ssword3", "Alice", "Johnson"}
         };
+    }
 
-        // Test for a valid registration
-        String result1 = testRegistrationWorker.registerUser(true, "us_r4", "Password123!", "Tom", "Hanks", userDatabase);
+    @After
+    public void tearDown() {
+        // Clean up after each test
+        testRegistrationWorker.setIsRegistered(false);
+    }
+
+    @Test
+    public void testRegisterUser_validRegistration() {
+        String result = testRegistrationWorker.registerUser(true, "us_r4", "Password123!", "Tom", "Hanks", userDatabase);
+
         assertTrue("Registration should succeed for valid input.", testRegistrationWorker.getIsRegistered());
-        assertEquals("Welcome Tom Hanks, it is great to have you join us.", result1);
+        assertEquals("Welcome Tom Hanks, it is great to have you join us.", result);
+    }
 
-        // Test for an invalid username
-        String result2 = testRegistrationWorker.registerUser(true, "us", "Password123!", "Invalid", "Username", userDatabase);
+    @Test
+    public void testRegisterUser_invalidUsername() {
+        String result = testRegistrationWorker.registerUser(true, "us", "Password123!", "Invalid", "Username", userDatabase);
+
         assertFalse("Registration should fail for an invalid username.", testRegistrationWorker.getIsRegistered());
-        assertEquals("Invalid Username. The username must contain an underscore (_) and be no more than 5 characters long.", result2);
+        assertEquals("Invalid Username. The username must contain an underscore (_) and be no more than 5 characters long.", result);
+    }
 
-        // Test for an invalid password
-        String result3 = testRegistrationWorker.registerUser(true, "us_r5", "password", "Invalid", "Password", userDatabase);
+    @Test
+    public void testRegisterUser_invalidPassword() {
+        String result = testRegistrationWorker.registerUser(true, "us_r5", "password", "Invalid", "Password", userDatabase);
+
         assertFalse("Registration should fail for an invalid password.", testRegistrationWorker.getIsRegistered());
-        assertEquals("Invalid Password. The password must contain a capital letter (A, B, C), a number (1, 2, 3), a special character (#, &, !), and be at least 8 characters long.", result3);
+        assertEquals("Invalid Password. The password must contain a capital letter (A, B, C), a number (1, 2, 3), a special character (#, &, !), and be at least 8 characters long.", result);
+    }
 
-        // Test for an existing username
-        String result4 = testRegistrationWorker.registerUser(true, "us_r2", "Password123!", "Username", "Exists", userDatabase);
+    @Test
+    public void testRegisterUser_existingUsername() {
+        String result = testRegistrationWorker.registerUser(true, "us_r2", "Password123!", "Username", "Exists", userDatabase);
+
         assertFalse("Registration should fail for an existing username.", testRegistrationWorker.getIsRegistered());
-        assertEquals("This Username already exists. Please try again with a different Username.", result4);
+        assertEquals("This Username already exists. Please try again with a different Username.", result);
     }
 }
+
 //--------------------------------------------------------------------------------------------------------------------//
 //--------------------------------------------------------EOF---------------------------------------------------------//
